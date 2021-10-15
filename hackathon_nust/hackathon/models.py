dfa09898e68a25d11665ffcf085b8a8f3fc4e4b5
vuyelwadr <vuyelwadr@gmail.com>
2021-10-15 21:29:18 +0200

    added model.py skeleton

--- hackathon_nust/hackathon/models.py
+++ hackathon_nust/hackathon/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+# Django models here. (DATABASE TABLES)
+
+
+class teaching_load(models.Model):
+    # User will be a foreign key field. Models will be linked using a user_id field
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=None, null=True)
+    courses = models.IntegerField(default=0)
+    honours_research_supervision = models.IntegerField(default=0)
+
+class courses(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=None, null=True)
+    
+    # define the types of sessions with first element in each tuple as the actual value to be set on the model, and the second element is the human-readable name
+    session_types = [('t','theory'), ('p','theory')]
+
+    name = models.CharField(max_length=50)
+    coordinating = models.BooleanField(default=False)
+    first_time = models.BooleanField(default=True)
+    sessions = models.CharField(max_length=1, choices=session_types)
+    groups = models.IntegerField(default=0)
