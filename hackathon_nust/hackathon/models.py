e5d3d44adabd32a1fd7cd4039984bdccc49b54b0
263man <48086019+263man@users.noreply.github.com>
2021-10-16 13:57:03 +0200

    Update models.py
    Update

--- hackathon_nust/hackathon/models.py
+++ hackathon_nust/hackathon/models.py
@@ -15,10 +15,11 @@ class courses(models.Model):
     
     # define the types of sessions with first element in each tuple as the actual value to be set on the model, and the second element is the human-readable name
     session_types = [('t','theory'), ('p','practical')]
+    expirience_types = [('fto','1st time teaching course'), ('fti','1st time teaching course at institution'),('exp','expirienced')]
 
     name = models.CharField(max_length=50)
     coordinating = models.BooleanField(default=False)
-    first_time = models.BooleanField(default=True)
+    expirience = models.CharField(max_length=3, choices= expirience_types)
     #We should inlude the nuance for first time teaching specifically at the institution,
     #  keeping in mind ofcourse that first_time=true automatically means true here
     sessions = models.CharField(max_length=1, choices=session_types)
@@ -28,7 +29,7 @@ class research_load(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=None, null=True)
     project = models.CharField(max_length=50)
     student = models.CharField(max_length=50)
-    proof_reasearch = models.FileField(upload_to='proof of research/')
+    proof_reasearch = models.FileField(upload_to='proof/research/')
 
 class admin_load(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=None, null=True)
@@ -39,5 +40,5 @@ class community_load(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=None, null=True)
 
     activity = models.CharField(max_length=50)
-    proof_community = models.FileField(upload_to='proof of community outreach/')
+    proof_community = models.FileField(upload_to='proof/community_outreach/')
 
